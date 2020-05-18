8a485a511a43c0df93fafe3aa492ef886f00c1f6
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2020-05-18 10:05:03 -0400

    version bump to 0.3.2 with tracer 1.17.0

--- set-versions.bat
+++ set-versions.bat
@@ -4,14 +4,14 @@ REM Set these version variables and run the script to change all the files which
 REM The site extension version
 set major=0
 set minor=3
-set patch=1
+set patch=2
 set version_postfix=-prerelease
 
 REM The agent version
 set agent_version=7.18.1
 
 REM The dotnet tracer version
-set tracer_version=1.16.3-prerelease
+set tracer_version=1.17.0
 
 REM *************************************************************
 REM All of the below code updates versions in files, do not touch
