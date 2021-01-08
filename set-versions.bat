6e4df12833a3367d8b24c38a53776534f19b09fa
colin-higgins <developerspike@gmail.com>
2021-01-08 13:37:59 -0500

    Fix hosted tracer zip

--- set-versions.bat
+++ set-versions.bat
@@ -4,7 +4,7 @@ REM Set these version variables and run the script to change all the files which
 REM The site extension version
 set major=100
 set minor=3
-set patch=18
+set patch=19
 set version_postfix=-prerelease
 
 REM The agent version
