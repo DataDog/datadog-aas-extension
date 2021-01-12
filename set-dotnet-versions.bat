3b471d933b201c409c824b3fb98be514d1946faa
colin-higgins <developerspike@gmail.com>
2021-01-12 13:55:54 -0500

    Up the minor version for auto upgrade

--- set-dotnet-versions.bat
+++ set-dotnet-versions.bat
@@ -3,7 +3,7 @@ REM Set these version variables and run the script to change all the files which
 
 REM The site extension version
 set major=100
-set minor=3
+set minor=4
 set patch=22
 set version_postfix=-prerelease
 
