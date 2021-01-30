41c3471cdc850dd33328a5b7428b51782a4962e4
Zach Montoya <zach.montoya@datadoghq.com>
2021-01-29 16:29:27 -0800

    Add "Stop-Process -Force -Name w3wp"

--- dotnet/content/install.ps1
+++ dotnet/content/install.ps1
@@ -4,3 +4,6 @@ $tracePipeId=([guid]::NewGuid().ToString().ToUpper())
 $statsPipeId=([guid]::NewGuid().ToString().ToUpper())
 ((Get-Content -path .\applicationHost.xdt -Raw) -replace "uniqueTracePipeId", "${tracePipeId}") | Set-Content -Path .\applicationHost.xdt
 ((Get-Content -path .\applicationHost.xdt -Raw) -replace "uniqueStatsPipeId", "${statsPipeId}") | Set-Content -Path .\applicationHost.xdt
+
+# Kill the running w3wp processes to make sure they restart
+Stop-Process -Force -Name w3wp
