88fbe436c5c6895717b2a5ad082dfdb1a2cdcb4f
colin-higgins <developerspike@gmail.com>
2021-02-01 12:22:31 -0500

    Fix versioning script for new scheme

--- set-dotnet-versions.bat
+++ set-dotnet-versions.bat
@@ -4,10 +4,9 @@ REM The site extension release version
 set major=1
 set minor=0
 set patch=0
-set version_postfix=''
+set version_postfix=
 
 REM Specialized version for development package, increment as necessary for testing
-set development_major=0
 set development_minor=1
 set development_patch=0
 
@@ -21,16 +20,16 @@ REM ****************************************************************************
 REM All of the below code updates versions in files, do not touch unless you wish to modify the structure of those files
 REM **************************************************************************************************************************
 
-set release_path_regex=v[0-9][0-9]?_[0-9]+_[0-9]+
+set release_path_regex=v[1-9][0-9]?_[0-9]+_[0-9]+
 set release_path_replacement=v%major%_%minor%_%patch%
-set development_release_path_regex=v[0-9][0-9][0-9]_[0-9]+_[0-9]+
-set development_release_path_replacement=v%development_major%_%development_minor%_%development_patch%
+set development_release_path_regex=v0_[0-9][0-9]?[0-9]?_[0-9][0-9]?[0-9]?
+set development_release_path_replacement=v0_%development_minor%_%development_patch%
 
 set gitlab_yml=.gitlab-ci.yml
 powershell -Command "(gc .\%gitlab_yml%) -replace '%development_release_path_regex%', '%development_release_path_replacement%' | Out-File -encoding ASCII .\%gitlab_yml%"
 powershell -Command "(gc .\%gitlab_yml%) -replace '%release_path_regex%', '%release_path_replacement%' | Out-File -encoding ASCII .\%gitlab_yml%"
 
-set version_regex=[0-9]+\.[0-9]+\.[0-9]+[\-a-zA-Z]*
+set version_regex=[1-9]+\.[0-9]+\.[0-9]+[\-a-zA-Z]*
 powershell -Command "(gc .\%gitlab_yml%) -replace '%version_regex%.+windows-tracer-home.zip', '%tracer_version%/windows-tracer-home.zip' | Out-File -encoding ASCII .\%gitlab_yml%"
 powershell -Command "(gc .\%gitlab_yml%) -replace 'agent-binaries-%version_regex%-1-x86_64.zip', 'agent-binaries-%agent_version%-1-x86_64.zip' | Out-File -encoding ASCII .\%gitlab_yml%"
 
@@ -39,13 +38,13 @@ set release_version=%major%.%minor%.%patch%%version_postfix%
 powershell -Command "(gc .\%release_nuget%) -replace '%version_regex%', '%release_version%' | Out-File -encoding ASCII .\%release_nuget%"
 
 set dev_nuget=dotnet\DevelopmentVerification.DdDotNet.Apm.nuspec
-set development_package_version=%development_major%.%development_minor%.%development_patch%%version_postfix%
+set development_package_version=0.%development_minor%.%development_patch%%version_postfix%
 powershell -Command "(gc .\%dev_nuget%) -replace '%version_regex%', '%development_package_version%' | Out-File -encoding ASCII .\%dev_nuget%"
 
 set application_host_transform=dotnet\content\applicationHost.xdt
 
 set ext_version_replace='DD_AAS_DOTNET_EXTENSION_VERSION"" value=\"%release_version%\" xdt:Locator'
-set ext_version_regex=DD_AAS_DOTNET_EXTENSION_VERSION. value..[0-9]+.[0-9]+.[0-9]+. xdt.Locator
+set ext_version_regex=DD_AAS_DOTNET_EXTENSION_VERSION. value..[0-9][0-9]?[0-9]?.[0-9][0-9]?[0-9]?.[0-9][0-9]?[0-9]?. xdt.Locator
 
 powershell -Command "(gc .\%application_host_transform%) -replace '%ext_version_regex%', %ext_version_replace% | Out-File -encoding ASCII .\%application_host_transform%"
 
