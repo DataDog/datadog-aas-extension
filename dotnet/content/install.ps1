da3fcd02929418b5a2fdd1973ad0d27ba01e32c9
colin-higgins <developerspike@gmail.com>
2021-02-01 16:43:57 -0500

    Comment out force-stop script

--- dotnet/content/install.ps1
+++ dotnet/content/install.ps1
@@ -5,4 +5,8 @@ $statsPipeId=([guid]::NewGuid().ToString().ToUpper())
 ((Get-Content -path .\applicationHost.xdt -Raw) -replace "uniqueTracePipeId", "${tracePipeId}") | Set-Content -Path .\applicationHost.xdt
 ((Get-Content -path .\applicationHost.xdt -Raw) -replace "uniqueStatsPipeId", "${statsPipeId}") | Set-Content -Path .\applicationHost.xdt
 
-.\force-stop-web-app.ps1 > force-stop-web-app-log.txt
+
+#### Do not call this script unless we have a way to request iisreset or similar.
+#### If we can gracefully shutdown w3wp.exe, then this is potentially useful.
+#### Netfx applications need to STOP the web app before installing this extension.
+#### .\force-stop-web-app.ps1 > force-stop-web-app-log.txt
