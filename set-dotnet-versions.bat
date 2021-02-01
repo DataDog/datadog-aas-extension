ec4634ddcea7dc2bc90fde315e86265ac86608c8
colin-higgins <developerspike@gmail.com>
2021-02-01 16:46:12 -0500

    Increment dev version

--- set-dotnet-versions.bat
+++ set-dotnet-versions.bat
@@ -8,7 +8,7 @@ set version_postfix=
 
 REM Specialized version for development package, increment as necessary for testing
 set development_minor=1
-set development_patch=3
+set development_patch=4
 
 REM The agent version to deploy
 set agent_version=7.25.0
