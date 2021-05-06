f033a39b675530a98717b1625023c05dd526600f
Zach Montoya <zach.montoya@datadoghq.com>
2021-05-06 09:11:03 -0700

    Modify validate-w3wp-stopped to remove the w3wp process lookup. This should always result in success because applicationHost.xdt.dd should be present and transformed into applicationHost.xdt, though this keeps the failure branch around in case of error

--- dotnet/content/validate-w3wp-stopped.ps1
+++ dotnet/content/validate-w3wp-stopped.ps1
@@ -19,68 +19,6 @@ if ([System.IO.File]::Exists('.\applicationHost.xdt'))
   return
 }
 
-Write-Output "Current PID: $PID"
-
-$w3wpProcesses=@(Get-Process w3wp)
-
-foreach ($w3wp in @($w3wpProcesses)) 
-{	
-
-  $w3wp_id=$w3wp.Id
-  # The variables match exactly, so we need to examine loaded modules
-  $w3wp_modules=$w3wp.Modules
-  
-  Write-Output "Examining ${w3wp_id} as shutdown candidate"
-  
-  Write-Output $w3wp
-
-  $wellknown_scm_module_count=0;
-  $minimum_scm_module_count=4;
-  
-  foreach ($loaded_module in $w3wp_modules) 
-  {	
-    $module_name=$loaded_module.ModuleName
-    Write-Output $module_name
-
-    if ($module_name.StartsWith("rasapi32.dll")) 
-	{
-	  $wellknown_scm_module_count++
-	}
-    elseif ($module_name.StartsWith("rasman.dll")) 
-	{
-	  $wellknown_scm_module_count++
-	}
-    elseif ($module_name.StartsWith("secur32.dll")) 
-	{
-	  $wellknown_scm_module_count++
-	}
-    elseif ($module_name.StartsWith("schannel.dll")) 
-	{
-	  $wellknown_scm_module_count++
-	}
-    elseif ($module_name.StartsWith("mskeyprotect.dll")) 
-	{
-	  $wellknown_scm_module_count++
-	}
-    elseif ($module_name.StartsWith("ncryptsslp.dll")) 
-	{
-	  $wellknown_scm_module_count++
-	}
-  }
-  
-  if ($wellknown_scm_module_count -gt $minimum_scm_module_count)
-  {
-    Write-Output "Skipping ${w3wp_id} with ${wellknown_scm_module_count} scm modules"
-	continue;
-  }
-
-  Write-Output "Failing install due to running web application."
-  
-  # Return to prevent success file from being created
-  return
-  
-}
-
 # If we are here, then the extension has successfully installed.
 # Allow the transform to apply
 Move-Item -Path '.\applicationHost.xdt.dd' -Destination '.\applicationHost.xdt' -Force
