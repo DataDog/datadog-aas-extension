707be27833327b8680ba7dcc8506a50d1ce225e2
Andrew Lock <andrew.lock@datadoghq.com>
2021-06-02 17:15:31 +0100

    Update to support v1.27.0 (#59)

--- dotnet/content/install.cmd
+++ dotnet/content/install.cmd
@@ -3,7 +3,7 @@ REM Entrypoint: https://github.com/projectkudu/kudu/blob/13824205c60a4bdb53896b9
 
 @echo off
 
-set version=v1.3.3
+set version=v1.3.4
 set log_prefix=%date% %time% ^[%version%^]
 set log_file=..\..\Datadog.AzureAppServices.DotNet-Install.txt
 
--- set-dotnet-versions.bat
+++ set-dotnet-versions.bat
@@ -3,18 +3,18 @@ REM Set these version variables and run the script to change all the files which
 REM The site extension release version
 set major=1
 set minor=3
-set patch=3
+set patch=4
 set version_postfix=
 
 REM Specialized version for development package, increment as necessary for testing
 set development_minor=1
-set development_patch=26
+set development_patch=27
 
 REM The agent version to deploy
 set agent_version=7.25.0
 
 REM The dotnet tracer version to deploy
-set tracer_version=1.26.3
+set tracer_version=1.27.0
 
 REM **************************************************************************************************************************
 REM All of the below code updates versions in files, do not touch unless you wish to modify the structure of those files
