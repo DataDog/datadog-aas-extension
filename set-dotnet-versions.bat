8a73b9c59b56e79310375ef492d15c83a43c7e66
Colin Higgins <colin-higgins@users.noreply.github.com>
2021-09-23 08:37:32 -0400

    Increment dev version to deploy script changes (#74)

--- set-dotnet-versions.bat
+++ set-dotnet-versions.bat
@@ -8,7 +8,7 @@ set version_postfix=
 
 REM Specialized version for development package, increment as necessary for testing
 set development_minor=1
-set development_patch=37
+set development_patch=38
 
 REM The agent version to deploy (Manually including 7.30 pre-upload)
 set agent_version=7.30.2
