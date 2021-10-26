1d487e06efe51a1d2381e53f4a32bbd0b8552990
Colin Higgins <colin-higgins@users.noreply.github.com>
2021-10-25 22:27:26 -0400

    WIP

--- helper-gui/Datadog.Kudu.Manager/Pages/Index.cshtml.cs
+++ helper-gui/Datadog.Kudu.Manager/Pages/Index.cshtml.cs
@@ -12,6 +12,12 @@ namespace Datadog.Kudu.Manager.Pages
 	{
 		private readonly ILogger<IndexModel> _logger;
 
+		public string ApiKey { get; set; }
+		public string Env { get; set; }
+		public string Service { get; set; }
+		public string Version { get; set; }
+		public string Site { get; set; }
+
 		public IndexModel(ILogger<IndexModel> logger)
 		{
 			_logger = logger;
@@ -19,7 +25,11 @@ namespace Datadog.Kudu.Manager.Pages
 
 		public void OnGet()
 		{
-
+			ApiKey = Environment.GetEnvironmentVariable("DD_API_KEY");
+			Env = Environment.GetEnvironmentVariable("DD_ENV");
+			Service = Environment.GetEnvironmentVariable("DD_SERVICE");
+			Version = Environment.GetEnvironmentVariable("DD_VERSION");
+			Site = Environment.GetEnvironmentVariable("DD_SITE");
 		}
 	}
 }
