78186acef954b26c83ab40837484262d484b8e18
Colin Higgins <colin-higgins@users.noreply.github.com>
2021-10-25 22:28:51 -0400

    Remove test code

--- helper-gui/Datadog.Kudu.Helper/Models/DatadogTroubleshootingModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Datadog.Kudu.Helper.Models
-{
-	public class DatadogTroubleshootingModel
-	{
-		public List<string> Problems { get; set; } = new List<string>();
-	}
-}
