75e5c8170ed4251c1162398d116fbf6c2c2cb498
Colin Higgins <colin-higgins@users.noreply.github.com>
2021-10-27 11:35:38 -0400

    Enable java tracer and logging

--- java/content/install.ps1
+++ java/content/install.ps1
@@ -9,3 +9,6 @@ $tracePipeId=([guid]::NewGuid().ToString().ToUpper())
 $statsPipeId=([guid]::NewGuid().ToString().ToUpper())
 ((Get-Content -path .\applicationHost.xdt -Raw) -replace "uniqueTracePipeId", "${tracePipeId}") | Set-Content -Path .\applicationHost.xdt
 ((Get-Content -path .\applicationHost.xdt -Raw) -replace "uniqueStatsPipeId", "${statsPipeId}") | Set-Content -Path .\applicationHost.xdt
+
+# Create log directory ahead of time
+New-Item -ItemType Directory -Force -Path ${env:HOME}\LogFiles\datadog\java\vFOLDERUNKNOWN
