39530d8c6e922a3a6b5e7ae2dcc1ea40a01d96e5
Colin Higgins <colin-higgins@users.noreply.github.com>
2021-11-10 16:27:50 -0500

    Refactor install and enable scm processes to submit

--- dotnet/content/install.cmd
+++ dotnet/content/install.cmd
@@ -1,24 +1,4 @@
 
 REM Entrypoint: https://github.com/projectkudu/kudu/blob/13824205c60a4bdb53896b9553ef1f370a93b911/Kudu.Core/SiteExtensions/SiteExtensionManager.cs#L548
-
-@echo off
-
-set version=vUNKNOWN
-set log_prefix=%date% %time% ^[%version%^]
-set log_file=..\..\Datadog.AzureAppServices.DotNet-Install.txt
-
-echo %log_prefix% Starting install. >> %log_file%
-
-IF EXIST ..\Datadog.AzureAppServices\applicationHost.xdt (
-  echo %log_prefix% Datadog.AzureAppServices.DotNet can not be installed side by side with Datadog.AzureAppServices. >> %log_file%
-  exit /B 2
-)
-
-IF EXIST .\applicationHost.xdt (
-  echo %log_prefix% Upgrade will not apply until full application stop. >> %log_file%
-)
-
-POWERSHELL .\install.ps1 -ExtensionVersion %version%
-
-echo %log_prefix% Successfully installed. >> %log_file%
-exit /B 0
+set log_file=..\..\LogFiles\datadog\Datadog.AzureAppServices.DotNet-Install.txt
+POWERSHELL .\install.ps1 >> %log_file%
