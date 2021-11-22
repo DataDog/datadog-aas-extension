5d862dc0d9eabc399fb106b24477ebdc1f3a5ff8
Colin Higgins <colin-higgins@users.noreply.github.com>
2021-11-22 14:09:14 -0500

    Add mkdir to install (#94)

--- dotnet/content/install.cmd
+++ dotnet/content/install.cmd
@@ -1,4 +1,5 @@
 
 REM Entrypoint: https://github.com/projectkudu/kudu/blob/13824205c60a4bdb53896b9553ef1f370a93b911/Kudu.Core/SiteExtensions/SiteExtensionManager.cs#L548
+mkdir ..\..\LogFiles\datadog
 set log_file=..\..\LogFiles\datadog\Datadog.AzureAppServices.DotNet-Install.txt
 POWERSHELL .\install.ps1 >> %log_file%
