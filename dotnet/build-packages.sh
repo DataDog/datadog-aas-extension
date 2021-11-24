7af0feaf1a4f6ad3e80d180a0f297b46ebb1a140
Colin Higgins <colin-higgins@users.noreply.github.com>
2021-11-24 15:00:08 -0500

    increment dotnet development version

--- dotnet/build-packages.sh
+++ dotnet/build-packages.sh
@@ -1,6 +1,6 @@
 
 RELEASE_VERSION="1.9.2"
-DEVELOPMENT_VERSION="0.1.53-prerelease"
+DEVELOPMENT_VERSION="0.1.54-prerelease"
 AGENT_DOWNLOAD_URL="http://s3.amazonaws.com/dsd6-staging/windows/agent7/buildpack/agent-binaries-7.32.0-1-x86_64.zip" 
 TRACER_DOWNLOAD_URL="https://github.com/DataDog/dd-trace-dotnet/releases/download/v1.30.1/windows-tracer-home.zip"
 
