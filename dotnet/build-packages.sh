8eb2b4a5ede6258bcdff54bad7c8d8ea0b962c1a
Pierre Bonet <pierre.bonet@datadoghq.com>
2022-02-02 14:53:51 +0100

    Updating the dev package version
    I had already uploaded the 0.1.60 one for tests

--- dotnet/build-packages.sh
+++ dotnet/build-packages.sh
@@ -1,6 +1,6 @@
 
 RELEASE_VERSION="2.2.100"
-DEVELOPMENT_VERSION="0.1.60-prerelease"
+DEVELOPMENT_VERSION="0.1.61-prerelease"
 AGENT_DOWNLOAD_URL="http://s3.amazonaws.com/dsd6-staging/windows/agent7/buildpack/agent-binaries-7.32.4-1-x86_64.zip"
 TRACER_DOWNLOAD_URL="https://github.com/DataDog/dd-trace-dotnet/releases/download/v2.2.0/windows-tracer-home.zip"
 
