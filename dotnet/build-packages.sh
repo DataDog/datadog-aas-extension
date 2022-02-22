0b30a361be856d1db1b6e05aa21180656e643293
andrewlock <andrewlock@users.noreply.github.com>
2022-02-22 14:29:46 +0000

    [.Net Version Bump] Bump to v2.4.000

--- dotnet/build-packages.sh
+++ dotnet/build-packages.sh
@@ -1,8 +1,8 @@
 
-RELEASE_VERSION="2.3.000"
-DEVELOPMENT_VERSION="0.2.101-prerelease"
-AGENT_DOWNLOAD_URL="http://s3.amazonaws.com/dsd6-staging/windows/agent7/buildpack/agent-binaries-7.32.4-1-x86_64.zip"
-TRACER_DOWNLOAD_URL="https://github.com/DataDog/dd-trace-dotnet/releases/download/v2.3.0/windows-tracer-home.zip"
+RELEASE_VERSION="2.4.000"
+DEVELOPMENT_VERSION="0.2.102-prerelease"
+AGENT_DOWNLOAD_URL="http://s3.amazonaws.com/dsd6-staging/windows/agent7/buildpack/agent-binaries-7.33.1-1-x86_64.zip"
+TRACER_DOWNLOAD_URL="https://github.com/DataDog/dd-trace-dotnet/releases/download/v2.4.0/windows-tracer-home.zip"
 
 echo "Downloading tracer from ${TRACER_DOWNLOAD_URL}"
 wget -O tracer.zip $TRACER_DOWNLOAD_URL
