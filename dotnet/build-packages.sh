fa85643b4b5ada12c730326cbb5bebddaaf50e52
Andrew Lock <andrew.lock@datadoghq.com>
2022-04-22 15:11:40 +0100

    Stop bumping the agent

--- dotnet/build-packages.sh
+++ dotnet/build-packages.sh
@@ -1,7 +1,7 @@
 
 RELEASE_VERSION="2.7.000"
 DEVELOPMENT_VERSION="0.2.110-prerelease"
-AGENT_DOWNLOAD_URL="http://s3.amazonaws.com/dsd6-staging/windows/agent7/buildpack/agent-binaries-7.35.1-1-x86_64.zip"
+AGENT_DOWNLOAD_URL="http://s3.amazonaws.com/dsd6-staging/windows/agent7/buildpack/agent-binaries-7.32.4-1-x86_64.zip"
 TRACER_DOWNLOAD_URL="https://github.com/DataDog/dd-trace-dotnet/releases/download/v2.7.0/windows-tracer-home.zip"
 
 echo "Downloading tracer from ${TRACER_DOWNLOAD_URL}"
