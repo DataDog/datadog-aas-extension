b5e8247be52f22ba772ed7401f63f85ace170456
Rey Abolofia <rey.abolofia@datadoghq.com>
2022-11-17 08:59:05 -0800

    Clean up directory structure.

--- node/linux/datadog-aas-node-linux/datadog-startup-script.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-export DD_TRACE_AGENT_PATH="/home/site/wwwroot/datadog-aas-node-linux/trace-agent"
-export NODE_OPTIONS='--require="dd-trace/init" --require="/home/site/wwwroot/datadog-aas-node-linux/start-trace-agent.js"'
