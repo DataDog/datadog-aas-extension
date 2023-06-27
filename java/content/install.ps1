0a044ddfb88f2a8f8af20650d2d60fc3bab3e8aa
Jordan <jordan.storms@datadoghq.com>
2023-06-27 16:14:33 -0400

    try new changes

--- java/content/install.ps1
+++ java/content/install.ps1
@@ -1,14 +1,35 @@
- param (
-    [Parameter(Mandatory=$true)][string]$ExtensionVersion
-)
-
 # https://github.com/projectkudu/kudu/blob/13824205c60a4bdb53896b9553ef1f370a93b911/Kudu.Services/SiteExtensions/SiteExtensionController.cs#L240
+
+$Version="vUNKNOWN"
+
+function Log ([string] $text)
+{
+    $LogDate=Get-Date -Format "yyyy-MM-dd HH:mm K"
+    Write-Output "[(${Version}) ${LogDate}] $text"
+}
+
+Log("Beginning install")
+
+function SetPipe ([string] $file, [string] $pipePattern, [string] $pipeGuid)
+{
+	if (Select-String -Path $file -Pattern $pipePattern -SimpleMatch -Quiet)
+	{
+		Log("Setting ${pipePattern} to ${pipeGuid} in ${file}")
+		((Get-Content -path $file -Raw) -replace $pipePattern, $pipeGuid) | Set-Content -Path $file
+	}
+	else
+	{
+		Log("${pipePattern} has already been set in ${file}")
+	}
+}
 
 # Set the unique pipe names in the applicationHost.xdt file for traces and metrics
 $tracePipeId=([guid]::NewGuid().ToString().ToUpper())
 $statsPipeId=([guid]::NewGuid().ToString().ToUpper())
-((Get-Content -path .\applicationHost.xdt -Raw) -replace "uniqueTracePipeId", "${tracePipeId}") | Set-Content -Path .\applicationHost.xdt
-((Get-Content -path .\applicationHost.xdt -Raw) -replace "uniqueStatsPipeId", "${statsPipeId}") | Set-Content -Path .\applicationHost.xdt
 
-# Create log directory ahead of time
-New-Item -ItemType Directory -Force -Path ${env:HOME}\LogFiles\datadog\java\vFOLDERUNKNOWN
+SetPipe ".\applicationHost.xdt" "uniqueStatsPipeId" "${statsPipeId}"
+SetPipe ".\applicationHost.xdt" "uniqueTracePipeId" "${tracePipeId}"
+SetPipe ".\scmApplicationHost.xdt" "uniqueStatsPipeId" "${statsPipeId}"
+SetPipe ".\scmApplicationHost.xdt" "uniqueTracePipeId" "${tracePipeId}"
+
+Log("Install complete")
