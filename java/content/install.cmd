a83f9ff0bfbdb8840071ff86732c1a4d912e1a36
Jordan <jordan.storms@datadoghq.com>
2023-09-20 10:48:01 -0400

    match working version

--- java/content/install.cmd
+++ java/content/install.cmd
@@ -1,5 +1,20 @@
 
 REM Entrypoint: https://github.com/projectkudu/kudu/blob/13824205c60a4bdb53896b9553ef1f370a93b911/Kudu.Core/SiteExtensions/SiteExtensionManager.cs#L548
-mkdir ..\..\LogFiles\datadog
-set log_file=..\..\LogFiles\datadog\java\vFOLDERUNKNOWN
-POWERSHELL .\install.ps1 >> %log_file%
+
+@echo off
+
+set version=vUNKNOWN
+set log_prefix=%date% %time% ^[%version%^]
+set log_file=..\..\Datadog.AzureAppServices.Java-Install.txt
+
+echo %log_prefix% Starting install. >> %log_file%
+
+IF EXIST .\applicationHost.xdt (
+  echo %log_prefix% Upgrade will not apply until full application stop. >> %log_file%
+)
+
+POWERSHELL .\install.ps1 -ExtensionVersion %version%
+
+echo %log_prefix% Successfully installed. >> %log_file%
+exit /B 0
+
