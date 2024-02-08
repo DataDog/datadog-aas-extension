2d68e34abb7f7ae5b5482a8bcdf6b5a1135e5fe9
Duncan Harvey <duncan.harvey@datadoghq.com>
2024-02-08 11:14:47 -0500

    minor edits to build script

--- node/build-packages.sh
+++ node/build-packages.sh
@@ -1,28 +1,24 @@
-# TODO: build dll on windows machine then investigate how to do so in a pipeline
-# TODO: build dlls for 32 bit and 64 bit
-# TODO: add code for agent process manager to repo
-
-ENV="dev"
+ENV="dev" # append "-e prod" when running script to override env
+RELEASE_VERSION="1.0.0"
 AGENT_VERSION="7.50.3"
 TRACER_VERSION="5.2.0"
 
-while getopts e:v:a:t: flag
-do
-    case "${flag}" in
-        e) ENV=${OPTARG};; # determines nuget package name and adds "prerelease" to version
-        v) VERSION=${OPTARG};; 
-        a) AGENT_VERSION=${OPTARG};;
-        t) TRACER_VERSION=${OPTARG};;
+while getopts e: opt; do
+    case $opt in
+    # env sets nuget package name and adds "prerelease" to version
+    e)
+        if [[ $OPTARG == "dev" || $OPTARG == "prod" ]]; then
+            ENV=${OPTARG}
+        else
+            echo "Invalid argument for -e option: '$OPTARG'. Allowed values are 'dev' or 'prod'" >&2
+            exit 1
+        fi
+        ;;
     esac
 done
 
-if [ -z "$VERSION" ]; then
-    echo "Must set semantic version"
-    exit 1
-fi
-
 if [ "$ENV" = "dev" ]; then
-    VERSION+="-prerelease"
+    RELEASE_VERSION+="-prerelease"
 fi
 
 AGENT_DOWNLOAD_URL="http://s3.amazonaws.com/dsd6-staging/windows/agent7/buildpack/agent-binaries-${AGENT_VERSION}-1-x86_64.zip"
@@ -30,7 +26,7 @@ AGENT_DOWNLOAD_URL="http://s3.amazonaws.com/dsd6-staging/windows/agent7/buildpac
 NUGET_DIR=node/content
 DOWNLOADS_DIR=node/downloads
 
-VERSION_FILE=$(echo ${VERSION} | tr '.' '_')
+VERSION_FILE=$(echo ${RELEASE_VERSION} | tr '.' '_')
 VERSION_DIR=$NUGET_DIR/v${VERSION_FILE}
 
 rm -rf $NUGET_DIR
@@ -42,7 +38,7 @@ npm install --prefix $VERSION_DIR/Tracer dd-trace@${TRACER_VERSION}
 echo "Downloading agent from ${AGENT_DOWNLOAD_URL}"
 wget -O $DOWNLOADS_DIR/agent.zip $AGENT_DOWNLOAD_URL
 if [ $? -ne 0 ]; then
-    exit 1;
+    exit 1
 fi
 
 unzip -o $DOWNLOADS_DIR/agent.zip -d $DOWNLOADS_DIR/node-agent-extract
@@ -60,9 +56,9 @@ echo "Versioning configuration files"
 sed -i "s/vFOLDERUNKNOWN/v${VERSION_FILE}/g" $VERSION_DIR/Agent/datadog.yaml
 sed -i "s/vFOLDERUNKNOWN/v${VERSION_FILE}/g" $VERSION_DIR/Agent/dogstatsd.yaml
 sed -i "s/vFOLDERUNKNOWN/v${VERSION_FILE}/g" $NUGET_DIR/applicationHost.xdt
-sed -i "s/vUNKNOWN/v${VERSION}/g" $NUGET_DIR/applicationHost.xdt
-sed -i "s/vUNKNOWN/v${VERSION}/g" $NUGET_DIR/install.cmd
-sed -i "s/vUNKNOWN/v${VERSION}/g" $NUGET_DIR/install.ps1
+sed -i "s/vUNKNOWN/v${RELEASE_VERSION}/g" $NUGET_DIR/applicationHost.xdt
+sed -i "s/vUNKNOWN/v${RELEASE_VERSION}/g" $NUGET_DIR/install.cmd
+sed -i "s/vUNKNOWN/v${RELEASE_VERSION}/g" $NUGET_DIR/install.ps1
 
 echo "Building process_manager"
 cd node/process_manager # TODO: fix so cd is not necessary
@@ -74,7 +70,7 @@ cp node/process_manager/target/x86_64-pc-windows-gnu/release/process_manager.exe
 
 echo "Creating nuget package"
 if [ "$ENV" = "dev" ]; then
-    dotnet pack node/DevelopmentVerification.DdNode.Apm.csproj -p:Version=${VERSION} -p:NoBuild=true -p:NoDefaultExcludes=true -o package
+    dotnet pack node/DevelopmentVerification.DdNode.Apm.csproj -p:Version=${RELEASE_VERSION} -p:NoBuild=true -p:NoDefaultExcludes=true -o package
 else
-    dotnet pack node/Datadog.AzureAppServices.Node.Apm.csproj -p:Version=${VERSION} -p:NoBuild=true -p:NoDefaultExcludes=true -o package
+    dotnet pack node/Datadog.AzureAppServices.Node.Apm.csproj -p:Version=${RELEASE_VERSION} -p:NoBuild=true -p:NoDefaultExcludes=true -o package
 fi
