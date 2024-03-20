20b08c5491b298c236b66605a9d3dbfef53453d7
Duncan Harvey <duncan.harvey@datadoghq.com>
2024-03-20 17:18:25 -0400

    remove aws networking for testing

--- node/scripts/signFiles.bat
+++ node/scripts/signFiles.bat
@@ -3,8 +3,6 @@ mkdir c:\build
 cd c:\build
 xcopy /y/e/s c:\mnt\*.* .
 
-powershell -C "c:\aws_networking.ps1"
-
 dd-wcs sign node\AgentProcessManager\x64\Release\AgentProcessManager.dll
 dd-wcs sign node\AgentProcessManager\Release\AgentProcessManager.dll
 dd-wcs sign node\process_manager\target\x86_64-pc-windows-gnu\release\process_manager.exe
