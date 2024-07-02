6c162778e921ef1d36b69b8b050e77b64416913e
Joey Zhao <5253430+joeyzhao2018@users.noreply.github.com>
2024-07-02 16:40:04 -0400

    agent 7.54.1

--- dotnet/build-packages-dev.sh
+++ dotnet/build-packages-dev.sh
@@ -1,5 +1,5 @@
 DEVELOPMENT_VERSION="0.3.0-prerelease"
-AGENT_DOWNLOAD_URL="http://s3.amazonaws.com/dsd6-staging/windows/agent7/buildpack/agent-binaries-7.54.0-1-x86_64.zip"
+AGENT_DOWNLOAD_URL="http://s3.amazonaws.com/dsd6-staging/windows/agent7/buildpack/agent-binaries-7.54.1-1-x86_64.zip"
 TRACER_DOWNLOAD_URL="https://apmdotnetci.blob.core.windows.net/apm-dotnet-ci-artifacts-master/INSTALL_SHA/windows-tracer-home.zip"
 
 
