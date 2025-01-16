6d18d7f13e2e193f7ae7275f84fa7e78b6f9af41
apiarian-datadog <apiarian-datadog@users.noreply.github.com>
2025-01-16 16:36:54 +0000

    [.Net Version Bump] Bump to v3.8.001

--- dotnet/build-packages.sh
+++ dotnet/build-packages.sh
@@ -1,5 +1,5 @@
-RELEASE_VERSION="3.8.000"
-AGENT_DOWNLOAD_URL="http://s3.amazonaws.com/dsd6-staging/windows/agent7/buildpack/agent-binaries-7.54.1-1-x86_64.zip"
+RELEASE_VERSION="3.8.001"
+AGENT_DOWNLOAD_URL="http://s3.amazonaws.com/dsd6-staging/windows/agent7/buildpack/agent-binaries-7.61.0-1-x86_64.zip"
 TRACER_DOWNLOAD_URL="https://github.com/DataDog/dd-trace-dotnet/releases/download/v3.8.0/windows-tracer-home.zip"
 
 echo "Downloading tracer from ${TRACER_DOWNLOAD_URL}"
