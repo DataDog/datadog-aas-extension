0b5edf56b5d9deeaf7d1d971758b40d6187acdef
Aleksandr Pasechnik <aleksandr.pasechnik@datadoghq.com>
2025-01-21 11:42:03 -0500

    chore: use a slightly older agent version

--- node/scripts/build-packages.sh
+++ node/scripts/build-packages.sh
@@ -3,7 +3,7 @@ if [[ -z ${RELEASE_VERSION+x} ]] || [[ -z ${DEVELOPMENT_VERSION+x} ]]; then
     exit 1
 fi
 
-AGENT_VERSION="7.61.0"
+AGENT_VERSION="7.60.1"
 TRACER_VERSION="4.55.0"
 
 echo "Building version ${RELEASE_VERSION} for prod environment"
