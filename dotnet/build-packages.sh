d297c867f8845176fb0a94361972c5e36fd36945
Andrew Lock <andrew.lock@datadoghq.com>
2025-05-06 12:12:47 +0200

    Update dotnet/build-packages.sh

--- dotnet/build-packages.sh
+++ dotnet/build-packages.sh
@@ -1,4 +1,4 @@
-RELEASE_VERSION="3.16.0"
+RELEASE_VERSION="3.16.000"
 AGENT_DOWNLOAD_URL="http://s3.amazonaws.com/dsd6-staging/windows/agent7/buildpack/agent-binaries-7.60.1-1-x86_64.zip"
 TRACER_DOWNLOAD_URL="https://github.com/DataDog/dd-trace-dotnet/releases/download/v3.16.0/windows-tracer-home.zip"
 
