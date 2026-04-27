49b32131489160c5c71f16ef514ce1852164f540
duncanpharvey <35278470+duncanpharvey@users.noreply.github.com>
2026-04-27 14:41:08 +0000

    [Java Extension] Bump tracer to

--- java/scripts/build-packages.sh
+++ java/scripts/build-packages.sh
@@ -4,7 +4,7 @@ if [[ -z ${RELEASE_VERSION+x} ]] || [[ -z ${DEVELOPMENT_VERSION+x} ]]; then
 fi
 
 AGENT_VERSION="7.77.0"
-TRACER_VERSION="1.61.1"
+TRACER_VERSION=""
 
 echo "Building version ${RELEASE_VERSION} for prod environment"
 echo "Building version ${DEVELOPMENT_VERSION} for dev environment"
